81f108e3a6a63e1f6a7111bf5b969093f6a4a165
Ajai Chandi <ajaischandi@gmail.com>
2024-05-20 11:55:15 -0400

    Functionality is complete

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from pytube import Playlist, YouTube, Channel
-import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
-from flask import Flask, request, url_for, session, redirect, render_template
-import csv
-
+from flask import Flask, request, url_for, session, redirect, render_template, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-clientID = 'e61032d8b5654366b5b33d91eede43ad'
-clientSecret = 'e755df87f9bf440d811c2f3ea305d95e'
-
 app.secret_key = "abcdefg"
-app.config['Session_Cookie_Name'] = "Ajai's Cookie"
 TOKEN_INFO = "token_info"
+
+# Define Spotify API credentials
+CLIENT_ID = 'e61032d8b5654366b5b33d91eede43ad'
+CLIENT_SECRET = 'e755df87f9bf440d811c2f3ea305d95e'
+REDIRECT_URI = 'http://127.0.0.1:5000/redirect'  # Update the redirect URI accordingly
 
 @app.route('/')
 def login():
@@ -30,102 +29,74 @@ def redirectPage():
     token_info = sp_oauth.get_access_token(code, check_cache=False)
     session[TOKEN_INFO] = token_info
     print("Token Info:", token_info)
-    return redirect(url_for('youtube_scrape', _external=True))
+    return redirect(url_for('youtube_scrape'))
 
-@app.route('/youtube_scrape', methods = ['POST', 'GET'])
+@app.route('/youtube_scrape', methods=['POST', 'GET'])
 def youtube_scrape():
-
     if request.method == 'POST':
-        link = request.form.get('link')
-        print("Link: "+ link+'\n')
+        link = request.json.get('link')
+        print("Link:", link)
+
         if link:
+            # Scrape YouTube playlist
             vlinks = Playlist(link)
+            playlist_title = vlinks.title
+            print("\n")
 
-            f = open('songs.csv', 'r+')
-            f.truncate(0)
-
-            filename = 'songs.csv'
-            f = open(filename, 'a')
-            headers = 'Artist,Song\n'
-            f.write(headers)
+            song_uris = []
 
             for song in vlinks:
-                
                 item = YouTube(song)
                 channel_url = item.channel_url
                 c = Channel(channel_url)
-                name = c.channel_name
-                name = name.split('-')[0].strip()
-                title = item.title.replace(",","")
-                name = name.replace(",","")
-                f.write(name +', '+ title +', ' + '\n')
-                print(name +" "+ title)
+                name = c.channel_name.split('-')[0].strip()
+                title = item.title.replace(",", "")
+                print(name + " " + title)
                 print("\n")
-            f.close()
-            
-            
-            try:
-                token_info = get_token()
-            except:
-                print("User not logged in")
-                return redirect('/')
-            
-            sp = spotipy.Spotify(auth=token_info['access_token'])
-            
-            
-            song_uris = []
-            user_id = sp.current_user()['id']
-            
-            csv_file = 'songs.csv'
-            with open(csv_file, 'r') as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    song_name = row['Song']
-                    artist_name = row['Artist']
+                song_uris.append(search_spotify(name, title))
 
-                    # Search for song
-                    results = sp.search(q=song_name +' '+ artist_name, type='track')
-                    
-                    # Get song URI if found
-                    if results['tracks']['items']:
-                        song_uri = results['tracks']['items'][0]['uri']
-                        song_uris.append(song_uri)
-                        print(f"Song: {song_name} by {artist_name}, URI: {song_uri}")
-                    else:
-                        print(f"Song: {song_name} by {artist_name} not found on Spotify")
-            file.close()
+            # Create Spotify playlist
+            sp = get_spotify_client()
+            playlist_id = create_spotify_playlist(sp, playlist_title, song_uris)
 
-            name = "Youtube Playlist"
-            yt_playlist = sp.user_playlist_create(user_id, name, True)
-            yt_playlist_id = yt_playlist['id']
-            sp.user_playlist_add_tracks(user_id, yt_playlist_id, song_uris)
-
-            return render_template('index.html', link=link)
-    
+            return jsonify({'message': 'Playlist created successfully', 'playlist_id': playlist_id})
+        else:
+            return jsonify({'error': 'No link provided'})
     else:
-        print("FAILED\n")
         return render_template('index.html')
 
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
+def search_spotify(artist_name, song_name):
+    sp = get_spotify_client()
+    results = sp.search(q=f'{song_name} {artist_name}', type='track')
 
+    if results['tracks']['items']:
+        return results['tracks']['items'][0]['uri']
+    else:
+        print(f"Song: {song_name} by {artist_name} not found on Spotify")
+        return None
+
+
+def get_spotify_client():
+    token_info = session.get(TOKEN_INFO, None)
     if not token_info:
         raise Exception("Token not found in session")
-    now = int(time.time())
 
-    is_expired = token_info['expires_at'] - now <60
-    if (is_expired):
-        sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-        session[TOKEN_INFO] = token_info
-    return token_info
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    return sp
+
+def create_spotify_playlist(sp, name, song_uris):
+    user_id = sp.current_user()['id']
+    playlist = sp.user_playlist_create(user_id, name, public=False)
+    sp.user_playlist_add_tracks(user_id, playlist['id'], song_uris)
+    return playlist['id']
 
 def create_spotify_oauth():
     return SpotifyOAuth(
-            client_id='e61032d8b5654366b5b33d91eede43ad',
-            client_secret='e755df87f9bf440d811c2f3ea305d95e',
-            redirect_uri=url_for('redirectPage', _external=True),
-            scope = "user-library-read playlist-modify-public playlist-modify-private")
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope="user-library-read playlist-modify-public playlist-modify-private"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
