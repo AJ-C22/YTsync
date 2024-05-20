4385186fadfe260b7884a73428d571ce14734061
Ajai Chandi <ajaischandi@gmail.com>
2024-05-20 12:03:24 -0400

    Added documentation

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ app = Flask(__name__)
 app.secret_key = "abcdefg"
 TOKEN_INFO = "token_info"
 
-# Define Spotify API credentials
+# Spotify API credentials
 CLIENT_ID = 'e61032d8b5654366b5b33d91eede43ad'
 CLIENT_SECRET = 'e755df87f9bf440d811c2f3ea305d95e'
-REDIRECT_URI = 'http://127.0.0.1:5000/redirect'  # Update the redirect URI accordingly
+REDIRECT_URI = 'http://127.0.0.1:5000/redirect' 
 
+#Initial OAUTH route
 @app.route('/')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
+#Spotify API rediret route
 @app.route('/redirect')
 def redirectPage():
     sp_oauth = create_spotify_oauth()
@@ -31,6 +33,7 @@ def redirectPage():
     print("Token Info:", token_info)
     return redirect(url_for('youtube_scrape'))
 
+#Scrapes YouTube Music Playlist
 @app.route('/youtube_scrape', methods=['POST', 'GET'])
 def youtube_scrape():
     if request.method == 'POST':
@@ -65,6 +68,7 @@ def youtube_scrape():
     else:
         return render_template('index.html')
 
+# Searches for Spotify song 
 def search_spotify(artist_name, song_name):
     sp = get_spotify_client()
     results = sp.search(q=f'{song_name} {artist_name}', type='track')
@@ -75,7 +79,7 @@ def search_spotify(artist_name, song_name):
         print(f"Song: {song_name} by {artist_name} not found on Spotify")
         return None
 
-
+# Spotify token verification and spotipy activation
 def get_spotify_client():
     token_info = session.get(TOKEN_INFO, None)
     if not token_info:
@@ -84,12 +88,14 @@ def get_spotify_client():
     sp = spotipy.Spotify(auth=token_info['access_token'])
     return sp
 
+# Creates a playlist using the song uris
 def create_spotify_playlist(sp, name, song_uris):
     user_id = sp.current_user()['id']
     playlist = sp.user_playlist_create(user_id, name, public=False)
     sp.user_playlist_add_tracks(user_id, playlist['id'], song_uris)
     return playlist['id']
 
+# Spotify OAuth
 def create_spotify_oauth():
     return SpotifyOAuth(
         client_id=CLIENT_ID,
